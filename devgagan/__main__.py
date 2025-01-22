0b54511fa2043037a6979cacae804a830b52c5d3
ak4422920 <ak4422920@gmail.com>
2025-01-22 19:55:33 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -41,9 +41,9 @@ async def devggn_boot():
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
 👨‍💻 Author: Gagan
-🌐 GitHub: https://github.com/devgaganin/
-📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
+🌐 GitHub: https://github.com
+📬 Telegram: https://t.me/akmovieshubbackup
+▶️ Botz: https://t.me/akmoviebotz
 🗓️ Created: 2025-01-11
 🔄 Last Modified: 2025-01-11
 🛠️ Version: 2.0.5
