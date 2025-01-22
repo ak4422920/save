444eb9534322a30b1cd20378e9a83c79c65ae41f
ak4422920 <ak4422920@gmail.com>
2025-01-22 21:14:21 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -222,7 +222,7 @@ async def see_plan(client, callback_query):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/The_SonGoku")],
         ]
     )
     await callback_query.message.edit_text(plan_text, reply_markup=buttons)
